420e51864e1bff0c68afd82f2800e3a9964e4a7a
williamzeni <william.zeni@mail.polimi.it>
2021-09-18 12:23:43 +0200

    lol

---
+++ src/main/java/it/polimi/tiw/tiw179/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.tiw179;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
