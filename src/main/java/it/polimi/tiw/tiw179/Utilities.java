4bf466d0c8775081ff193578fcd3fb64456c88cc
williamzeni <william.zeni@mail.polimi.it>
2021-10-19 14:59:11 +0200

    pulizia

--- src/main/java/it/polimi/tiw/tiw179/Utilities.java
+++ src/main/java/it/polimi/tiw/tiw179/Utilities.java
@@ -62,15 +62,6 @@ public class Utilities {
         }
     }
 
-    public static void idExist(Connection connection, int id){
 
-    }
-
-    // TODO: 10/6/21 completare il tutto graficamente con CSS
-    // TODO: 10/7/21 creare un box per le scelte possibili del padre durante l'aggiunta di un topic 
-    // TODO: 10/6/21 vedere se è possibile imporre un trigger che aggiunge automaticamente la subtopic quando ha un padre
-    // TODO: 10/6/21 aggiungere un check sul fatto che l'id del topic non possa essere divisibile per 10 sul database (fatto sul server)
-    // TODO: 10/6/21 implementare la funzione di spostamento
     // TODO: 10/6/21 a database ultimato verificare come fare upload del database su github
-    // TODO: 10/13/21 gestire le modifiche/aggiunte contemporanee con refresh della pagina
 }
